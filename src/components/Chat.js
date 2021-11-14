6a69cc9e6e2903b10ede1f8cde4452372fe4dfe9
orojlo <a.orojlo@gmail.com>
2021-11-14 23:17:33 +0330

    Chat And Room Data STR

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { useParams } from "react-router";
 import "./Chat.css";
+import useRoom from '../hooks/useRoom';
 
-export default function Chat() {
+
+export default function Chat([user, page]) {
+  const { roomId } = useParams()
+
+
+  const room = useRoom(roomId, user.uid)
+
+  console.log({ room });
+
+
   return <div className="chat">Chat</div>;
 }
